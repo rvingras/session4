bf42dd2d23d725983ada85336ff93418df3d2213
rvingras <raimondasvingras@yahoo.com>
2016-10-20 16:09:25 +0100

    -

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+
+
+    function onLoad() {
+      
+      var key = "pen";
+        var value = "blue";
+        
+            window.localStorage.setItem(key,value);
+            window.localStorage.getItem(key);
+            window.alert(key);
+        
+    }
+	
+	
